03526c131afbe83436b160baee6e843354199f8b
SethPattee <patteeseth@gmail.com>
2023-08-23 08:16:26 -0600

    2 hours done

---
+++ Chpt2/BoardGame.cs
@@ -0,0 +1,11 @@
+﻿namespace Chpt2
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? Year { get; set; }
+    }
+}
